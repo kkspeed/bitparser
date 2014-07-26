b73f76cda48f3b4e64e65a6676a28d81eaf34d13
Bruce Li <leilmyxwz@gmail.com>
2014-07-26 01:35:01 -0400

    Skeleton of parser

--- Data/Bitstream/Parser.hs
+++ Data/Bitstream/Parser.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Bitstream.Parser
-    ( satisfy, bit1Bool
+    ( satisfy, bit1Bool, bits, getBits, getWord8, getWord16Be, getWord32Be, getWord64Be
+    , getWord1
     ) where
 
 import Text.Parsec.Pos
@@ -8,8 +9,12 @@ import Text.Parsec.Prim
 import Text.Printf
 
 import Text.Parsec.Bitstream.Lazy
+
 import qualified Data.Bitstream.Lazy as LS
+import qualified Data.Bits as Bits
 import Data.Word
+
+import Control.Applicative ((<$>))
 
 satisfy :: (LS.Bitstream LS.Right -> Bool) -> Parser (LS.Bitstream LS.Right)
 satisfy f = tokenPrim format
@@ -21,3 +26,36 @@ satisfy f = tokenPrim format
 
 bit1Bool :: Bool -> Parser (LS.Bitstream LS.Right)
 bit1Bool t = satisfy ((==t) . LS.head)
+
+bits :: (Bits.Bits a, Integral a, Show a) => a -> Parser (LS.Bitstream LS.Right)
+bits bts = do
+  r <- getBits (Bits.bitSize bts)
+  if LS.toBits r == bts
+  then return r
+  else fail $ "Does not match: " ++ show bts
+
+getBits :: Int -> Parser (LS.Bitstream LS.Right)
+getBits n = do
+  State input pos user <- getParserState
+  let bbs = LS.take n input
+  let rst = LS.drop n input
+  let l' = LS.length bbs
+  _ <- setParserState $ State rst (incSourceColumn pos l') user
+  if (l' < n)
+  then fail "Unexpected EOF!"
+  else return bbs
+
+getWord1 :: Parser Word8
+getWord1 = LS.toBits <$> getBits 1
+
+getWord8 :: Parser Word8
+getWord8 = LS.toBits <$> getBits 8
+
+getWord16Be :: Parser Word16
+getWord16Be = LS.toBits <$> getBits 16
+
+getWord32Be :: Parser Word32
+getWord32Be = LS.toBits <$> getBits 32
+
+getWord64Be :: Parser Word64
+getWord64Be = LS.toBits <$> getBits 64
